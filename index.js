a506a3557580322b027b9b8ca93fe0eb0a75c96d
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2023-04-23 15:13:58 +0600

    category wise news filter complete

--- index.js
+++ index.js
@@ -3,17 +3,43 @@ const app = express();
 const cors =require('cors')
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
 app.use(cors())
+
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
 
 app.get('/news-categories' , (req, res)=>{
     res.send(categories);
 })
 
+app.get('/category/:id' ,(req , res)=>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n=>n.category_id === id);
+        res.send(categoryNews);
+    }
+  
+})
+
+app.get('/news/:id',(req , res)=>{
+    const id = req.params.id;
+    const selectedNews =news.find(n => n._id === id);
+        res.send(selectedNews);
+    
+})
+
+
+
+
+
+
 
 app.get('/' , (req , res)=>{
     res.send('News API is running')
-
 });
 
 app.listen(port ,()=>{
